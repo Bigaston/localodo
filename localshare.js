7037362541e7ce1f2a49b08f9a501323f13aad04
Bigaston <bigaston1@gmail.com>
2020-07-21 11:11:05 +0200

    feat: upload

--- localshare.js
+++ localshare.js
@@ -8,7 +8,8 @@ const ifaces = os.networkInterfaces();
 const mustache = require("mustache")
 const fs = require("fs");
 const path = require("path");
-const { url } = require("inspector");
+const multer  = require('multer')
+var upload = multer()
 
 let ip_tab = [];
 let ip_name = [];
@@ -52,15 +53,19 @@ function startServ() {
 	app.use("/export", express.static("export"))
 
 	app.get("/", (req, res) => {
-		QRCode.toDataURL(`http://${IP}:${process.env.PORT}/download`, function (err, url) {
-			let render_obj = {
-				qrcode_url: url,
-				share_addr: `http://${IP}:${process.env.PORT}/download`
-			}
-
-			let template = fs.readFileSync(path.join(__dirname, "./web/index.mustache"), "utf8");
-
-			res.send(mustache.render(template, render_obj));
+		QRCode.toDataURL(`http://${IP}:${process.env.PORT}/download`, function (err, url_download) {
+			QRCode.toDataURL(`http://${IP}:${process.env.PORT}/download`, function (err, url_share) {
+				let render_obj = {
+					qrcode_download: url_download,
+					download_addr: `http://${IP}:${process.env.PORT}/download`,
+					qrcode_share: url_share,
+					share_addr: `http://${IP}:${process.env.PORT}/upload`
+				}
+	
+				let template = fs.readFileSync(path.join(__dirname, "./web/index.mustache"), "utf8");
+	
+				res.send(mustache.render(template, render_obj));
+			})
 		})
 	})
 	
@@ -74,6 +79,18 @@ function startServ() {
 		let template = fs.readFileSync(path.join(__dirname, "./web/download.mustache"), "utf8");
 
 		res.send(mustache.render(template, render_obj));
+	})
+
+	app.get("/upload", (req, res) => {
+		res.sendFile(path.join(__dirname, "web/upload.html"));
+	})
+
+	app.post('/uploadfile', upload.array('file'), function (req, res, next) {
+		req.files.forEach(f => {
+			fs.writeFileSync(path.join(__dirname, "import/" + f.originalname), f.buffer);
+		})
+
+		res.redirect("/")
 	})
 	
 	app.listen(process.env.PORT, IP, () => {
